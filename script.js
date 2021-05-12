2cd19b3c50d9b83c36258f08ffa0467509842a0a
c-henry1 <78730639+c-henry1@users.noreply.github.com>
2021-05-12 12:20:24 +0100

    Final
    Final

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-var slideIndex = 0;
+var slideIndex1 = 0;
   carousel();
   
   function carousel() {
@@ -9,9 +9,9 @@ var slideIndex = 0;
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
-    x[slideIndex-1].style.display = "block";
+    slideIndex1++;
+    if (slideIndex1 > x.length) {slideIndex1 = 1}
+    x[slideIndex1-1].style.display = "block";
     setTimeout(carousel, 1000); // Change image every 2 seconds
   }
 
@@ -29,3 +29,79 @@ var slideIndex = 0;
     x[slideIndex-1].style.display = "block";
     setTimeout(carouselr, 1000); // Change image every 2 seconds
   }
+
+  
+
+  const navSlide = () => {
+      const tour = document.querySelector('#tour');
+      const nav = document.querySelector('.dates-container');
+
+    tour.addEventListener('click',() =>{
+        nav.classList.toggle('dates-container-active');
+        })
+    }
+
+  navSlide();
+
+
+  const textChange = () => {
+    const tour = document.querySelector('#tour');
+
+  tour.addEventListener('click',() =>{
+    if (tour.innerHTML === "/ Tour /") {
+        tour.innerHTML = "/ X /";
+     } else {
+        tour.innerHTML = "/ Tour /";
+     }
+      })
+  }
+
+  textChange();
+
+  
+
+
+  //document.getElementById('tour').onclick = function tourDiv () {
+    //  document.getElementById('dates-container').style.display = "flex";
+      //document.getElementById('tour').style.display = "none"
+    //}
+
+
+    //document.getElementsById('close').onclick = function closeTour () {
+      //  document.getElementById('dates-container').style.display = "none";
+        //document.getElementById('tour').style.display = "flex"
+      
+   
+
+    //  document.getElementsById('manufa').onclick = function closeTour () {
+      //  document.getElementById('dates-container').style.display = "none";
+        //document.getElementById('tour').style.display = "flex"
+      
+  
+    
+
+
+//let tourDates = document.querySelector("dates-container")
+//let datesButton = document.querySelector("#tour")
+
+//datesButton.addEventListener("click", () => {
+
+  //  showDates = !showDates
+    //if (showDates === true) {
+ //       tourDates.style.display = "flex"
+   // } else {
+     //   tourDates.style.display = "none"
+    //}
+
+    //datesButton.innerText = buttonStates[datesButton.innerText]
+
+
+//})
+
+//let showDates = false
+
+//let buttonStates = {
+  //  "/ Tour /" : "/ X /",
+    //"/ X /" : "/ Tour /"
+//}
+  
